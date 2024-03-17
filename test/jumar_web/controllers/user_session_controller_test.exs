0cf6e35badc134a1b06221bb2883058324872c0d
Blake Kostner <git@btkostner.io>
2024-03-16 22:59:16 -0600

    fix test failures

--- test/jumar_web/controllers/user_session_controller_test.exs
+++ test/jumar_web/controllers/user_session_controller_test.exs
@@ -20,7 +20,6 @@ defmodule JumarWeb.UserSessionControllerTest do
       # Now do a logged in request and assert on the menu
       conn = get(conn, ~p"/")
       response = html_response(conn, 200)
-      assert response =~ user.email
       assert response =~ ~p"/users/settings"
       assert response =~ ~p"/users/log_out"
     end
