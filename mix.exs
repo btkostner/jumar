eeb5ec232a5053a0f0e593fa7d8d8bc515cc7876
Blake Kostner <git@btkostner.io>
2024-04-13 04:06:17 +0000

    chore(deps): update dependency finch to ~> 0.18

--- mix.exs
+++ mix.exs
@@ -69,7 +69,7 @@ defmodule Jumar.MixProject do
       {:ecto_sql, "~> 3.6"},
       {:esbuild, "~> 0.5", runtime: Mix.env() == :dev},
       {:ex_doc, "~> 0.27", only: :dev, runtime: false},
-      {:finch, "~> 0.13"},
+      {:finch, "~> 0.18"},
       {:floki, "~> 0.30", only: :test},
       {:gettext, "~> 0.20"},
       {:heroicons, "~> 0.5"},
